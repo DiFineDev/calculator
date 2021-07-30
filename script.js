9662659af7d748bccad5040805dcc4cf4b6f0c3a
Dmitry Komarov <dakom455@icloud.com>
2021-07-30 15:42:36 +0400

    Выполнил урок 2

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 60000; // Доход за месяц
+let income = 'Фриланс'; // Дополнительные доходы
+let addExpenses = 'Интернет, такси, коммуналка'; // Дополнительные расходы
+let deposit = true;
+let mission = 140000; // Необходимая сумма накоплений
+let period = 10; // Количесво месяцев
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission+ ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log('Дневной бюджет: ' + budgetDay + ' руб');
+
 
 alert('Hello');
 
