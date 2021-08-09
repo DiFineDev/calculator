a355b32705f9da21499fa814869f95a76f698469
Dmitry Komarov <dakom455@icloud.com>
2021-08-09 19:20:34 +0400

    complete homework

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const buttonOfCalculation = document.getElementById('start');
+const buttonIncomeAdd = document.getElementsByTagName('button')[0];
+const buttonExpensesAdd = document.getElementsByTagName('button')[1];
+const checkboxOfDeposit = document.querySelector('#deposit-check');
+const entryFields = document.querySelectorAll('.additional_income-item');
+
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+const budgetDayValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value'); 
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value'); 
+const incomePeriodValue = document.getElementsByClassName('income_period-value'); 
+const targetMonthValue = document.getElementsByClassName('target_month-value'); 
+
+const  salaryAmount = document.querySelector('.salary-amount');
+const  incomeTitle = document.querySelector('.income-title');
+const  incomeAmount = document.querySelector('.income-amount');
+const  expensesTitle = document.querySelector('.expenses-title');
+const  expensesAmount = document.querySelector('.expenses-amount');
+const  additionalExpensesItem= document.querySelector('.additional_expenses-item');
+const  targetAmount = document.querySelector('.target-amount');
+const  periodSelect = document.querySelector('.period-select');
+
 function start(){  
     do {
         money = prompt('Ваш месячный доход?', '120000');
